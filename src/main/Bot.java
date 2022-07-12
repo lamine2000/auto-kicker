88d0d458297744472d9e41b36560e1c3c552657d
lamine2000 <lemzogueye.lg72@gmail.com>
2022-07-12 05:42:25 +0000

    auto kicker select events test

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -3,16 +3,13 @@ package main;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
-import net.dv8tion.jda.api.events.role.update.GenericRoleUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
-import java.util.EnumSet;
 
 public class Bot extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
@@ -36,17 +33,7 @@ public class Bot extends ListenerAdapter {
     }
 
     @Override
-    public void onGenericRoleUpdate(@NotNull GenericRoleUpdateEvent event) {
-        System.out.println("OGU "+ event);
-    }
-
-    @Override
-    public void onGuildMemberRoleAdd(@NotNull GuildMemberRoleAddEvent event) {
-        System.out.println("OGMRA "+ event);
-    }
-
-    @Override
-    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
-        System.out.println("OGMJ " +event);
+    public void onGenericEvent(@NotNull GenericEvent event) {
+        System.out.println(event);
     }
 }
