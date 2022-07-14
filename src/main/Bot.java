199323c0b9d9059609f958225c9e5b3536604212
lamine2000 <lemzogueye.lg72@gmail.com>
2022-07-14 15:33:39 +0000

    Adding choosing timer utilities

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -59,12 +59,12 @@ public class Bot extends ListenerAdapter {
 
         EmbedBuilder eb = new EmbedBuilder();
 
-        eb.setTitle(String.format("Greetings, %s!!", event.getUser().getAsTag()),  null);
+        eb.setTitle(String.format("Greetings, %s!!", event.getUser().getAsTag()),  event.getUser().getEffectiveAvatarUrl());
         eb.setColor(new Color(0xf44336));
         eb.setDescription("Here are our rules:\n1- Don't blala\n2- Try and bleble first\n3- Have fun !");
         eb.setAuthor("The Moderators", "https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/zekroBot_Logo_-_round_small.png");
         eb.setFooter("Have fun !", "https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/zekroBot_Logo_-_round_small.png");
-        eb.setImage(event.getUser().getAvatarUrl());
+        eb.setImage("https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/logo%20-%20title.png");
         eb.setThumbnail("https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/logo%20-%20title.png");
         eb.setTimestamp(Instant.now());
 
